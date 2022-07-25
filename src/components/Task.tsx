9c18a6b5bc9520ce7989a9ff1603f40c8116b7f2
Matheus <matheuus412@gmail.com>
2022-07-25 16:28:45 -0300

    feat: Mostrar o progresso de conclusão das tarefas

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,54 +1,117 @@
-import styles from './Task.module.css'
-import { Trash } from "phosphor-react";
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import { PlusCircle, Trash } from "phosphor-react";
 import Clipboard from '../assets/Clipboard.svg'
-import { useState } from 'react';
+import styles from './Task.module.css'
+import { v4 as uuidv4 } from 'uuid';
 
 interface TaskProps {
-    content: string;
-    whenDeleteTask: (task: string) => void
+    id: string,
+    title: string;
+    isCompleted: boolean
 }
 
-export function Task({ content, whenDeleteTask }: TaskProps) {
-    
+export function Task() {
+    const [tasks, setTasks] = useState<TaskProps[]>([]);
+    const [newTask, setNewTask] = useState('')
 
-    const [isCompleted, setIsCompleted] = useState(false)
-
-    function handleDeleteTask() {
-        whenDeleteTask(content)
+    const task: TaskProps = {
+        id: uuidv4(),
+        title: newTask,
+        isCompleted: false
     }
 
-    function handleCompleteTask() {
-        isCompleted == true ? setIsCompleted(false) : setIsCompleted(true)
+    function handleNewTask(event: FormEvent) {
+        event.preventDefault();
+        setTasks([...tasks, task])
     }
 
-    {
-        if (content != null) {
-
-            return (
-                <div className={styles.task}>
-                    <input
-                        className={styles.gridItem}
-                        type="checkbox"
-                        defaultChecked={isCompleted}
-                        onClick={handleCompleteTask} />
-                    <p className={isCompleted ? styles.taskCompleted : ''}>{content}</p>
-                    <button
-                        onClick={handleDeleteTask}
-                        className={styles.gridItem}>
-                        <Trash />
-
-                    </button>
-                </div>
-
-            )
-        }
+    //Método que monitora é "pega" o valor que é digitado no input
+    function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+        setNewTask(event.target.value);
     }
+
+    function handleDeleteTask(taskToDelete: string) {
+        const tasksWithoutDeletedOne = tasks.filter(tarefa => {
+            return tarefa.id != taskToDelete
+        })
+        setTasks(tasksWithoutDeletedOne);
+    }
+
+
+
+    function handleCompleteTask(completeTask: string) {
+        const taskCompleted = tasks.map(tarefa => {
+            if (tarefa.id == completeTask) {
+                tarefa.isCompleted = !tarefa.isCompleted
+            }
+            return tarefa
+
+        })
+        console.log(taskCompleted);
+        setTasks(taskCompleted)
+
+    }
+
+    const completedTotal = tasks.filter(tarefa => tarefa.isCompleted);
+    console.log(completedTotal)
+
+
     return (
+        <main>
+            <form onSubmit={handleNewTask} className={styles.inputArea} >
+                <input
+                    type="text"
+                    placeholder="Adicione uma nova tarefa"
+                    onChange={handleNewTaskChange} />
 
-        <div className={styles.taskList}>
-            <img src={Clipboard} alt="" />
-            <strong>Você ainda não tem tarefas cadastradas</strong>
-            <p>Crie tarefas e organize seus itens a fazer</p>
-        </div >
+                <button
+                    type="submit">
+                    Criar
+                    <PlusCircle size={15.97} />
+                </button>
+            </form>
+
+            <div className={styles.labels}>
+                <p>Tarefas criadas <span>{tasks.length}</span></p>
+                {tasks.length <= 0 ?
+                    <p>Concluídas <span>0</span></p>
+                    : <p>Concluídas <span>{completedTotal.length} de {tasks.length}</span></p>}
+
+            </div>
+            <div className={styles.taskList}>
+
+                {tasks.length <= 0 &&
+                    <>
+                        <img src={Clipboard} alt="" />
+                        <strong>Você ainda não tem tarefas cadastradas</strong>
+                        <p>Crie tarefas e organize seus itens a fazer</p>
+                    </>
+
+                }
+
+                {tasks.map(task => {
+
+                    return (
+                        <div className={styles.task} key={task.id}>
+                            <input
+                                type="checkbox"
+                                defaultChecked={task.isCompleted}
+                                onClick={() => handleCompleteTask(task.id)} />
+
+                            <p className={task.isCompleted ? styles.taskCompleted : ''}>{task.title}</p>
+
+                            <button
+                                onClick={() => handleDeleteTask(task.id)}>
+                                <Trash />
+
+                            </button>
+                        </div>
+                    )
+                })}
+            </div>
+        </main >
     )
 }
+
+
+
